71d9c3e60e4e5d038d14c24c2a5ccf15bcae3b94
syehahmad <ahmadsangatrajinbelajar@gmail.com>
2026-07-10 00:02:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-function generate(){
+function generate() {
+    const tiket = document.getElementById("tiket").value;
+    const nama = document.getElementById("nama").value;
+    const sid = document.getElementById("sid").value;
+    const layanan = document.getElementById("layanan").value;
+    const rootcause = document.getElementById("rootcause").value;
+    const action = document.getElementById("action").value;
+    const snkabel = document.getElementById("snkabel").value;
+    const snont = document.getElementById("snont").value;
+    const apc = document.getElementById("apc").value;
+    const upc = document.getElementById("upc").value;
+    const sleeve = document.getElementById("sleeve").value;
+    const pigtail = document.getElementById("pigtail").value;
+    const tikoruser = document.getElementById("tikoruser").value;
+    const tikorputus = document.getElementById("tikorputus").value;
 
-const hasil=`FORMAT TIKET CLOSE
+    document.getElementById("hasil").value =
+`FORMAT TIKET CLOSE
 ====================================
-Tiket/Insiden : ${tiket.value}
+Tiket/Insiden : ${tiket}
 Tim : KENDAL
-Nama : ${nama.value}
-SID : ${sid.value}
-Layanan : ${layanan.value} Mbps
-Rootcause : ${rootcause.value}
-Action : ${action.value}
+Nama : ${nama}
+SID : ${sid}
+Layanan : ${layanan} Mbps
+Rootcause : ${rootcause}
+Action : ${action}
 
 Material Terpakai
 -------------------
-SN Kabel : ${snkabel.value}
-SN ONT : ${snont.value}
-Pathcord APC : ${apc.value}
-Pathcord UPC : ${upc.value}
-Slevee Protektor : ${sleeve.value}
-Pigtail : ${pigtail.value}
+SN Kabel : ${snkabel}
+SN ONT : ${snont}
+Pathcord APC : ${apc}
+Pathcord UPC : ${upc}
+Slevee Protektor : ${sleeve}
+Pigtail : ${pigtail}
 ====================================
-TIKOR USER : ${tikoruser.value}
-TIKOR TITIK PUTUS : ${tikorputus.value}`;
-
-document.getElementById("hasil").value=hasil;
-}
-
-function copyText(){
-const hasil=document.getElementById("hasil");
-navigator.clipboard.writeText(hasil.value);
-alert("Berhasil disalin");
+TIKOR USER : ${tikoruser}
+TIKOR TITIK PUTUS : ${tikorputus}`;
 }
